9726bd3630d9547b0b313850988aa9d5adf698bd
Alexicl89 <alex.geo.kpp@icloud.com>
2023-10-29 14:58:59 +0300

    Create MTSDTO

---
+++ src/main/java/com/dev/alex/phonecollect/model/Mts/MtsRootDTO.java
@@ -0,0 +1,9 @@
+package com.dev.alex.phonecollect.model.Mts;
+
+import com.dev.alex.phonecollect.model.BaseDTO;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties (ignoreUnknown = true)
+public class MtsRootDTO extends BaseDTO {
+
+}
